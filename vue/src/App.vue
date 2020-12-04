a3df62a55d4a69751047af60e71b7ed85fcfeec9
Jaeoon <JaeoonLee1@gmail.com>
2020-12-04 16:01:39 -0500

    Formatting Front-End. No Major Changes from Base

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,26 +2,32 @@
      Note that you have classes from bootstrap available to you.
      See https://getbootstrap.com/docs/4.5/getting-started/introduction/ for reference on bootstrap -->
 <template>
-  <div id="app" class="container"> <!-- If you start to get random styling you don't like, remove container from this div -->
+  <div id="app" class="container">
+    <!-- If you start to get random styling you don't like, remove container from this div -->
     <div id="nav">
       <router-link class="nav-item" v-bind:to="{ name: 'home' }">
-        <i class="fas fa-home"></i> <!-- This is a font awesome icon -->
-        Home 
+        <i class="fas fa-home"></i>
+        <!-- This is a font awesome icon -->
+        Home
       </router-link>
       <router-link
         class="nav-item"
         v-bind:to="{ name: 'register' }"
-        v-if="!$store.state.token">&nbsp;|&nbsp;Register</router-link>
+        v-if="!$store.state.token"
+        >&nbsp;|&nbsp;Register</router-link
+      >
       <router-link
         class="nav-item"
         v-bind:to="{ name: 'login' }"
-        v-if="!$store.state.token">
+        v-if="!$store.state.token"
+      >
         &nbsp;|&nbsp;Login
       </router-link>
       <router-link
         class="nav-item"
         v-bind:to="{ name: 'logout' }"
-        v-if="$store.state.token">
+        v-if="$store.state.token"
+      >
         &nbsp;|&nbsp;Logout
       </router-link>
     </div>
@@ -30,13 +36,9 @@
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <!-- Application-Wide Styles go here. 
      Any valid CSS or SCSS is allowable here. See https://sass-lang.com/documentation/style-rules for info on SCSS -->
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
