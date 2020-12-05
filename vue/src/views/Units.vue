68ccea578c61f184df87c220ee1ad0daeeb743bc
Jaeoon <JaeoonLee1@gmail.com>
2020-12-05 15:26:25 -0500

    changed formatting on login credentials

--- vue/src/views/Units.vue
+++ vue/src/views/Units.vue
@@ -8,7 +8,7 @@
       v-bind:key="u.id"
       v-bind:unit="u"
     >
-    <div></div>
+    <div>{ u.highBid }</div>
     </UnitCard>
 
     <p>Segment to add unit (Part of the "Unit Information" Card)</p>
