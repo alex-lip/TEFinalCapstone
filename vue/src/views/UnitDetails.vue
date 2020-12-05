d930b7f4375c4c371bcfee37f4cbe4c71ece1c2c
Clint <clinteversole@gmail.com>
2020-12-05 14:39:08 -0500

    added unit details, syntax to delete units

--- vue/src/views/UnitDetails.vue
+++ vue/src/views/UnitDetails.vue
@@ -19,7 +19,27 @@ created(){
     if(!this.unitDetails){
         this.unitDetails = this.$store.state.units.find(u => u.id == this.$route.params.id);
     }
- }
+ },
+   methods: {
+    deleteUnit(id){
+      const confirmed = confirm(
+        "Are you sure you want to delete this unit? This cannot be undone"
+      );
+
+      if (confirmed) {
+        unitService
+            .deleteUnit(id)
+            .then((response) => {
+              console.debug('Unit deleted', response);
+
+              if(response.status === 200){
+               //Call out to our web service and have it make the delete
+               this.$store.commit("UNIT_DELETED", this.unit);
+               this.$router.push({ name: "Units" });
+              }
+        });
+      }
+    },
 }
 </script>
 
