41d4ffe3bb8e932e403039f2b7243d4ba4521648
Jaeoon <JaeoonLee1@gmail.com>
2020-12-07 15:09:25 -0500

    Formatting and red-squiggly fix on UnitDetails.vue

--- vue/src/views/UnitDetails.vue
+++ vue/src/views/UnitDetails.vue
@@ -1,48 +1,49 @@
 <template>
   <div>
-      <h3>{{unitDetails.unitNumber}}</h3>
-      <h3>{{unitDetails.unitSize}}</h3>
-      <h3>{{unitDetails.locationName}}</h3>
+    <h3>{{ unitDetails.unitNumber }}</h3>
+    <h3>{{ unitDetails.unitSize }}</h3>
+    <h3>{{ unitDetails.locationName }}</h3>
   </div>
 </template>
 
 <script>
 export default {
-name: 'unit-details',
-data(){
-    return{
-        unitDetails: undefined,
+  name: "unit-details",
+  data() {
+    return {
+      unitDetails: undefined,
     };
-},
-created(){
-    this.unitDetails = this.$store.state.units.find(u => u.id == this.$route.params.id);
-    if(!this.unitDetails){
-        this.unitDetails = this.$store.state.units.find(u => u.id == this.$route.params.id);
+  },
+  created() {
+    this.unitDetails = this.$store.state.units.find(
+      (u) => u.id == this.$route.params.id
+    );
+    if (!this.unitDetails) {
+      this.unitDetails = this.$store.state.units.find(
+        (u) => u.id == this.$route.params.id
+      );
     }
- },
-   methods: {
-    deleteUnit(id){
+  },
+  methods: {
+    deleteUnit(id) {
       const confirmed = confirm(
         "Are you sure you want to delete this unit? This cannot be undone"
       );
 
       if (confirmed) {
-        unitService
-            .deleteUnit(id)
-            .then((response) => {
-              console.debug('Unit deleted', response);
+        unitService.deleteUnit(id).then((response) => {
+          console.debug("Unit deleted", response);
 
-              if(response.status === 200){
-               //Call out to our web service and have it make the delete
-               this.$store.commit("UNIT_DELETED", this.unit);
-               this.$router.push({ name: "Units" });
-              }
+          if (response.status === 200) {
+            //Call out to our web service and have it make the delete
+            this.$store.commit("UNIT_DELETED", this.unit);
+            this.$router.push({ name: "Units" });
+          }
         });
       }
     },
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
