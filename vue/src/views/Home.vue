5c6805e80ccd0727bbc943e32ff92a46793afbed
Clint <clinteversole@gmail.com>
2020-12-12 18:40:53 -0500

    edit home page

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <h1>Welcome to Self Storage Auctions</h1>
+    <p>Accept terms and conditions below to</p>
+    <p>see the full of units up for auction</p>
+    <p/>
+    <p>
+      <input type="checkbox" name="terms" v-on:click="checked = !checked">
+      I accept the terms and conditions
+    </p>
+    <router-link v-bind:to="{ name: 'units' }">
+    <button v-if=checked>I Accept</button>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+
+  data() {
+    return {
+      checked: false
+    }
+  }
 };
 </script>
