f0ce6f7615e6e4d816ef45e545761294d9907027
Clint <clinteversole@gmail.com>
2020-12-11 23:15:03 -0500

    started bids controller, DAO, and interface

---
+++ dotnet/Capstone/DAO/IBidDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.DAO
+{
+    public class IBidDAO
+    {
+       /*TODO: fix syntax to accept methods below
+        List<Bid> GetBids();
+
+        Bid GetBidById(int bid_id);
+
+        void DeleteBid(Bid bid);
+
+        bool AddBid(Bid bid);
+
+        bool EditBid(int id, Bid bid);
+        */
+    }
+}
