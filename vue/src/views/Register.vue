06470426d6927a6167be727ad3f7fe3e5fc08cb1
Alex <alex.lipinsky@gmail.com>
2020-12-13 12:07:38 -0500

    Registration page styling

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,15 +1,14 @@
 <template>
-<div class="container-fluid vh-100">
-  <div class="row align-items-start vh-100 overlay">
-    <div class="col-md-1 .ml-md-auto">Column 1</div>
+<div class="container-fluid ">
+  <div class="row align-items-start overlay">
+    <div class="col-md-1 .ml-md-auto"></div>
     <div class="col-md-5 .ml-md-auto" id="aboutContent">
-      <h1>Best Place to Bid on Abandoned Self Storage Units</h1>
-      <p>Bid on the contents of abandoned self storage units.</p>
-      <p>Register today to participate in online auctions.</p>
-      <p>Real time auctions.</p>
-      <p>Same day local pickup.</p>
+      <h1>Bid on Abandoned Self Storage Units</h1>
+      <p class="headerCopy">Bid on the contents of abandoned self storage units.
+      Register today to participate in online auctions.
+      Real time auctions. Same day local pickup.</p>
     </div>
-    <div class="col-md-1 .ml-md-auto">Column 3</div>
+    <div class="col-md-1 .ml-md-auto"></div>
   <div id="register" class="col text-center">
     <form class="form-register" @submit.prevent="register">
       <h1 class="h3 mb-3 font-weight-normal">Register</h1>
@@ -59,9 +58,32 @@
       </div>
     </form>
   </div>
-  <div class="col-md-1 .ml-md-auto">Column 4</div>
+  <div class="col-md-1 .ml-md-auto"></div>
   </div>
+<div class="container features">
+    <!-- Example row of columns -->
+    <div class="row">
+      <div class="col-md-4">
+      <span class="icons">
+        <i class="fas fa-clock"></i>
+      </span>
+        <h3>Real Time Bidding</h3>
+      <p class="featureCopy">Never lose at the last minute. Watch bids as they are placed.</p></div>
+      <div class="col-md-4">
+        <span class="icons">
+        <i class="fas fa-truck"></i>
+        </span>
+      <h3>Same Day Pickup</h3>
+      <p class="featureCopy">Get your winnings the same day. Pick up at our central Ohio locations.</p></div>
+      <div class="col-md-4">
+        <span class="icons">
+        <i class="fas fa-list-ol"></i>
+        </span>
+      <h3>Monitor Your Auctions</h3>
+      <p class="featureCopy">Keep track of your bids on multiple auctions in real time.</p></div>
+    </div>
   </div>
+</div>
 
 </template>
 
@@ -119,35 +141,40 @@ export default {
 <style>
 
 .container-fluid {
-  background-image: url(https://images.unsplash.com/photo-1595050309250-3047a857b88d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1987&q=80);
+  /*background-image: url(https://images.unsplash.com/photo-1595050309250-3047a857b88d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1987&q=80);
   background-position: center;
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: cover; */
   
   height: 100%;
+  
   
 }
 
 .overlay {
-    background: rgba(0, 0, 0, 0.8); /*can be anything, of course*/
+    /*background: rgba(0, 0, 0, 0.8); 
     background-blend-mode: multiply;
-    padding-top: 35px;
+    padding-top: 35px; */
+  
+  background-position-y: 20%;
+  background: linear-gradient(60deg, rgb(245, 104, 10) 28%, rgb(253, 227, 177) 100%);
 }
 
 #aboutContent {
   text-align: center;
-  background:rgb(47, 47, 47, 0.6);
   color: white;
   border-radius: 5px;
   padding: 1px;
-  margin-top:80px;
+  margin-top:50px;
+  margin-bottom:25px;
 }
 
 #register {
-  background:white;
+  background: (0,0,0,0.5);
   border-radius: 10px;
   padding: 15px;
-  margin-top:80px;
+  margin-top:50px;
+  margin-bottom:25px;
 }
 
 .btnGroup {
@@ -155,11 +182,25 @@ export default {
 }
 
 h1 {
-  font-size: 2.5em;
+  font-size: 4.0rem;
+  font-weight: 500;
 }
 
-p {
-  font-size: 1.5em;
+.icons {
+  color: #ff944d;
+  font-size: 3.0rem;
+}
+
+.features {
+  padding-top: 30px;
+  text-align: left;
+  margin-top: 20px;
+  margin-bottom: 40px;
+}
+
+.headerCopy  {
+  font-size: 1.5rem;
+  font-weight: 200;
   text-align: left;
   padding-left: 20px;
 }
