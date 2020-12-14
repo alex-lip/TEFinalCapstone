2382fd0f906c1bb4b654c2a7ffd772497a2f6a53
Alex <alex.lipinsky@gmail.com>
2020-12-14 09:25:14 -0500

    Styling terms & conditions page

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -138,7 +138,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 
 .container-fluid {
   /*background-image: url(https://images.unsplash.com/photo-1595050309250-3047a857b88d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1987&q=80);
